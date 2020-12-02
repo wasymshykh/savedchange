8ff81bca1a912c3fea6b3ce8360592c89139fafa
Wasym <46717121+wasymshykh@users.noreply.github.com>
2020-12-03 00:55:29 +0500

    initial website setup

---
+++ views/pages/home.view.php
@@ -0,0 +1,6 @@
+<div>
+    <h3>Create an account - <a href="<?=URL?>/signup/consumer.php">as consumer</a> <span>or</span>
+    <a href="<?=URL?>/signup/sales.php">as sales rep</a></h3>
+    
+    <h3>Login to account - <a href="<?=URL?>/login">go</a></h3>
+</div>
